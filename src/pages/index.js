7feeccdb05eca878efbb8b91b179d08f55247fd5
Mario Krstevski <mariokrstevski@hotmail.com>
2019-08-10 22:49:53 +0200

    Playing around with Gatsby trying out ideas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,33 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Toggle from "../components/toggle";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const loggedIn = true;
+
+const IndexPage = () => {
+
+  !loggedIn && navigate('/login');
+  
+  return  (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Toggle></Toggle>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/login/">Login</Link>
+    </Layout>
+  )
+  
+}
+
 
 export default IndexPage
---
+++ src/pages/login.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Link, navigate } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const LoginPage = () => (
+  <>
+    <SEO title="Login Page" />
+    
+    <div>
+    <h1>Any place in your app!</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          navigate("/")
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+
+    <Link to="/">Go back to the homepage</Link>
+  </>
+)
+
+export default LoginPage
