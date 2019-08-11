ea5769f0b4bf65b5095648769cad3cc89fa76cc3
Mario Krstevski <mariokrstevski@hotmail.com>
2019-08-11 13:36:15 +0200

    Created mock js file db with users

---
+++ src/utils/user-database.js
@@ -0,0 +1,32 @@
+export const users = [
+    {
+        username: "BoldStudent",
+        email: "student@mail.com",
+        name: "John",
+        surname: "Doe",
+        token:  "123456",
+        role:  "student",
+        isAuth: false,
+        password:  "student123",
+    },
+    {
+        username: "BoldDonor",
+        email :"donor@mail.com",
+        name: "Jane",
+        surname: "Dooh",
+        token:  "asdfgh",
+        role:  "donor",
+        isAuth: false,
+        password:  "donor123",
+    },
+    {
+        username: "Admin",
+        email :"admin@mail.com",
+        name: "Admin",
+        surname: "Admin",
+        token:  "asd123",
+        role:  "admin",
+        isAuth: false,
+        password:  "admin",
+    }
+]
